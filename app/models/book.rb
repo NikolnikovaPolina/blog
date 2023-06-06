e1e5906f344aedd63129a67583dbc89b5f3db9be
NikolnikovaPolina <nikolnikova03@mail.ru>
2023-06-06 19:45:58 +0300

    Adding a review

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
     validates :title, presence:true
     validates :description, presence:true, length: { minimum: 10}
+    has_many :reviews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_062209) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_163106) do
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
-    t.text "description", null: false 
+    t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "commenter", null: false
+    t.text "feedback", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+  end
+
+  add_foreign_key "reviews", "books"
 end
